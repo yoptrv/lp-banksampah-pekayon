c9fccd8c44ec2374fb57b10a5fe64cb1a4dbec37
Dio putra <dio50btl@gmail.com>
2026-01-25 23:51:43 +0700

    metadata

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,23 @@
+import Head from "next/head";
+
 import HeroSection from "@/components/sections/HeroSection";
 import JadwalSection from "@/components/sections/JadwalSection";
-import TutorSection from "@/components/sections/TutorSection";  
+import TutorSection from "@/components/sections/TutorSection";
 import KategoriSampahSection from "@/components/sections/KategoriSampahSection";
 import FAQSection from "@/components/sections/FaqSection";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Urai • Bank Sampah Kelurahan Pekayon</title>
+        <meta
+          name="description"
+          content="URAI - Bank Sampah Kelurahan Pekayon. Program lingkungan untuk pengelolaan sampah yang modern dan berkelanjutan."
+        />
+        <link rel="icon" href="/logo.png" />
+      </Head>
+
       <main>
         <HeroSection />
         <JadwalSection />
